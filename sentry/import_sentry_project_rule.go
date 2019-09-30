8aec020f0aaad0b637ff0ef4409e387e61d85130
chreble <christopheeble@gmail.com>
2019-09-30 17:39:25 +0200

    Added rules support

---
+++ sentry/import_sentry_project_rule.go
@@ -0,0 +1,27 @@
+package sentry
+
+import (
+	"errors"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceSentryRuleImporter(d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	addrID := d.Id()
+
+	log.Printf("[DEBUG] Importing rule using ADDR ID %s", addrID)
+
+	parts := strings.Split(addrID, "/")
+
+	if len(parts) != 3 {
+		return nil, errors.New("Project import requires an ADDR ID of the following schema org-slug/project-slug/rule-id")
+	}
+
+	d.Set("organization", parts[0])
+	d.Set("project", parts[1])
+	d.SetId(parts[2])
+
+	return []*schema.ResourceData{d}, nil
+}
