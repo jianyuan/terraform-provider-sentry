0cb149b277c07799fee1a5d38d633cba772ea9d4
Clint Byrum <clint@fewbar.com>
2020-03-11 16:33:13 -0700

    Fix wrong wording in error msg on import team
    The copy-pasta was just a little too saucy.

--- sentry/import_sentry_team.go
+++ sentry/import_sentry_team.go
@@ -16,7 +16,7 @@ func resourceSentryTeamImport(d *schema.ResourceData, meta interface{}) ([]*sche
 	parts := strings.Split(addrID, "/")
 
 	if len(parts) != 2 {
-		return nil, errors.New("Project import requires an ADDR ID of the following schema org-slug/project-slug")
+		return nil, errors.New("Project import requires an ADDR ID of the following schema org-slug/team-slug")
 	}
 
 	d.Set("organization", parts[0])
