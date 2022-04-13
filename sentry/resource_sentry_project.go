b852f55dadbf4f55b953f614ad9315ce283766fa
Foad Nosrati Habibi <foad.nh@gmail.com>
2022-04-14 08:51:14 +1000

    Change replace to direct path

--- sentry/resource_sentry_project.go
+++ sentry/resource_sentry_project.go
@@ -5,10 +5,10 @@ import (
 	"errors"
 	"strings"
 
+	"github.com/canva/go-sentry/sentry"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/jianyuan/go-sentry/sentry"
 )
 
 func resourceSentryProject() *schema.Resource {
