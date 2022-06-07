547f746fba76fdf76e72335d56afc3f70bc04694
Jian Yuan Lee <jianyuan@gmail.com>
2022-06-07 20:32:36 +0100

    Update kitchen sink

--- examples/kitchen-sink/sentry.tf
+++ examples/kitchen-sink/sentry.tf
@@ -1,45 +1,45 @@
 provider "sentry" {
-    token = "ba4c11e226cf4c0c914e1057dea1649bc7f9a5e993ec4e9aaa4de6cbd0944e34"
-    base_url = "http://localhost:9000/api/"
+  token    = "ba4c11e226cf4c0c914e1057dea1649bc7f9a5e993ec4e9aaa4de6cbd0944e34"
+  base_url = "http://localhost:9000/api/"
 }
 
 resource "sentry_organization" "my_organization" {
-    name = "My Organization"
-    agree_terms = true
+  name        = "My Organization"
+  agree_terms = true
 }
 
 resource "sentry_team" "engineering" {
-    organization = "${sentry_organization.my_organization.id}"
-    name = "The Engineering Team"
+  organization = sentry_organization.my_organization.id
+  name         = "The Engineering Team"
 }
 
 resource "sentry_project" "web_app" {
-    organization = "${sentry_team.engineering.organization}"
-    team = "${sentry_team.engineering.id}"
-    name = "Web App"
+  organization = sentry_team.engineering.organization
+  team         = sentry_team.engineering.id
+  name         = "Web App"
 }
 
 // Using the first parameter
 data "sentry_key" "via_first" {
-    organization = "${sentry_project.web_app.organization}"
-    project = "${sentry_project.web_app.id}"
-    first = true
+  organization = sentry_project.web_app.organization
+  project      = sentry_project.web_app.id
+  first        = true
 }
 
 // Using the name parameter
 data "sentry_key" "via_name" {
-    organization = "${sentry_project.web_app.organization}"
-    project = "${sentry_project.web_app.id}"
-    name = "Default"
+  organization = sentry_project.web_app.organization
+  project      = sentry_project.web_app.id
+  name         = "Default"
 }
 
 output "sentry_key_dsn_secret" {
-    value = "${data.sentry_key.via_name.dsn_secret}"
+  value = data.sentry_key.via_name.dsn_secret
 }
 
 resource "sentry_alert_rule" "default" {
-  organization      = "${sentry_organization.my_organization.id}"
-  project           = "${sentry_project.web_app.id}"
+  organization      = sentry_organization.my_organization.id
+  project           = sentry_project.web_app.id
   name              = "test-alert-rule"
   dataset           = "transactions"
   query             = "http.url:http://testservice.com/stats"
@@ -57,7 +57,7 @@ resource "sentry_alert_rule" "default" {
   }
 
   triggers {
-    actions = []
+    actions           = []
     alert_threshold   = 500
     label             = "warning"
     resolve_threshold = 100.0
