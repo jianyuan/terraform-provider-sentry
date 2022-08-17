cebec591ac363245d448654ca0d0a02d670b4715
danielpoonwj <daniel.poon.wenjie@gmail.com>
2022-08-17 18:07:43 +0800

    Add data source: sentry_organization_integration

---
+++ sentry/data_source_sentry_organization_integration.go
@@ -0,0 +1,85 @@
+package sentry
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jianyuan/go-sentry/v2/sentry"
+)
+
+func dataSourceSentryOrganizationIntegration() *schema.Resource {
+	return &schema.Resource{
+		Description: "Sentry Organization Integration data source.",
+
+		ReadContext: dataSourceSentryOrganizationIntegrationRead,
+
+		Schema: map[string]*schema.Schema{
+			"organization": {
+				Description: "The slug of the organization the integration belongs to.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"provider_key": {
+				Description: "The key of the organization integration provider.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"name": {
+				Description: "The name of the organization integration.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"internal_id": {
+				Description: "The internal ID for this organization integration.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func dataSourceSentryOrganizationIntegrationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*sentry.Client)
+
+	org := d.Get("organization").(string)
+	providerKey := d.Get("provider_key").(string)
+	integrationName := d.Get("name").(string)
+
+	tflog.Debug(ctx, "Reading organization integration", map[string]interface{}{"org": org, "provider_key": providerKey, "name": integrationName})
+
+	// get all paginated integrations with the provider key
+	var orgIntegrations []*sentry.OrganizationIntegration
+	params := &sentry.ListOrganizationIntegrationsParams{
+		ListCursorParams: sentry.ListCursorParams{},
+		ProviderKey:      providerKey,
+	}
+	for {
+		keys, resp, err := client.OrganizationIntegrations.List(ctx, org, params)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		orgIntegrations = append(orgIntegrations, keys...)
+
+		tflog.Debug(ctx, "Requested organization integration list cursor", map[string]interface{}{"cursor": resp.Cursor})
+		if resp.Cursor == "" {
+			break
+		}
+		params.ListCursorParams.Cursor = resp.Cursor
+	}
+
+	// filter for first matching name
+	for _, orgIntegration := range orgIntegrations {
+		if orgIntegration.Name == integrationName {
+			d.SetId(orgIntegration.ID)
+			retErr := multierror.Append(
+				d.Set("internal_id", orgIntegration.ID),
+			)
+			return diag.FromErr(retErr.ErrorOrNil())
+		}
+	}
+
+	return diag.Errorf("Can't find Sentry Organization Integration: %s", integrationName)
+}
--- sentry/provider.go
+++ sentry/provider.go
@@ -49,12 +49,13 @@ func NewProvider(version string) func() *schema.Provider {
 			},
 
 			DataSourcesMap: map[string]*schema.Resource{
-				"sentry_dashboard":    dataSourceSentryDashboard(),
-				"sentry_issue_alert":  dataSourceSentryIssueAlertSentryIssueAlert(),
-				"sentry_key":          dataSourceSentryKey(),
-				"sentry_metric_alert": dataSourceSentryMetricAlert(),
-				"sentry_organization": dataSourceSentryOrganization(),
-				"sentry_team":         dataSourceSentryTeam(),
+				"sentry_dashboard":                dataSourceSentryDashboard(),
+				"sentry_issue_alert":              dataSourceSentryIssueAlertSentryIssueAlert(),
+				"sentry_key":                      dataSourceSentryKey(),
+				"sentry_metric_alert":             dataSourceSentryMetricAlert(),
+				"sentry_organization":             dataSourceSentryOrganization(),
+				"sentry_organization_integration": dataSourceSentryOrganizationIntegration(),
+				"sentry_team":                     dataSourceSentryTeam(),
 			},
 		}
 
