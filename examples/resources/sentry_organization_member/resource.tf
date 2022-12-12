95a59e3483b2e7f9c3756ae7f8e9e5cdf77c7d3b
Jian Yuan Lee <jianyuan@gmail.com>
2022-12-12 17:08:51 +0000

    fix: sentry_organization_member was missing the required organization field

--- examples/resources/sentry_organization_member/resource.tf
+++ examples/resources/sentry_organization_member/resource.tf
@@ -1,5 +1,7 @@
 # Create an organization member
 resource "sentry_organization_member" "john_doe" {
+  organization = "my-organization"
+
   email = "test@example.com"
   role  = "member"
   teams = ["my-team"]
