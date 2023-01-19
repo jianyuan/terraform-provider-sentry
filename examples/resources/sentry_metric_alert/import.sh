1e66be739a1c2d72f90bc01cf008e40a5d2be5a0
Jian Yuan Lee <jianyuan@gmail.com>
2023-01-19 20:14:34 +0000

    Update import examples

--- examples/resources/sentry_metric_alert/import.sh
+++ examples/resources/sentry_metric_alert/import.sh
@@ -1,4 +1,6 @@
 # import using the organization, project slugs and rule id from the URL:
 # https://sentry.io/organizations/[org-slug]/projects/[project-slug]/
 # https://sentry.io/organizations/[org-slug]/alerts/rules/details/[rule-id]/
+# or
+# https://sentry.io/organizations/[org-slug]/alerts/metric-rules/[project-slug]/[rule-id]/
 terraform import sentry_metric_alert.default org-slug/project-slug/rule-id
