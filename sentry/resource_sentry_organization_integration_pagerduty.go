8ac4d29528034d444953563fd3c306dc17ca6e2f
Steve Brudz <sbrudz@gmail.com>
2023-04-11 16:37:27 -0500

    Implement sentry_organization_integration_pagerduty resource

---
+++ sentry/resource_sentry_organization_integration_pagerduty.go
@@ -0,0 +1,288 @@
+package sentry
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/jianyuan/go-sentry/v2/sentry"
+)
+
+func resourceSentryOrganizationIntegrationPagerduty() *schema.Resource {
+	return &schema.Resource{
+		Description: "Sentry PagerDuty Organization Integration resource.",
+
+		CreateContext: resourceSentryOrganizationIntegrationPagerdutyCreate,
+		ReadContext:   resourceSentryOrganizationIntegrationPagerdutyRead,
+		UpdateContext: resourceSentryOrganizationIntegrationPagerdutyUpdate,
+		DeleteContext: resourceSentryOrganizationIntegrationPagerdutyDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: importSentryOrganizationIntegrationPagerduty,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"organization": {
+				Description: "The slug of the Sentry organization this resource belongs to.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"integration_id": {
+				Description: "The organization integration ID for PagerDuty.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"service_name": {
+				Description: "The name of the PagerDuty service.",
+				Type:        schema.TypeString,
+				Required:    true,
+			},
+			"integration_key": {
+				Description: "The integration key from PagerDuty to associate with service_name.",
+				Type:        schema.TypeString,
+				Required:    true,
+				Sensitive:   true,
+			},
+			"internal_id": {
+				Description: "The internal ID for this PagerDuty service integration.",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func resourceSentryOrganizationIntegrationPagerdutyCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*sentry.Client)
+
+	org := d.Get("organization").(string)
+	integrationId := d.Get("integration_id").(string)
+	serviceName := d.Get("service_name").(string)
+	integrationKey := d.Get("integration_key").(string)
+
+	tflog.Debug(ctx, "Creating PagerDuty service integration", map[string]interface{}{
+		"org":            org,
+		"integration_id": integrationId,
+		"service_name":   serviceName,
+	})
+	orgIntegration, _, err := client.OrganizationIntegrations.Get(ctx, org, integrationId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	configData := *orgIntegration.ConfigData
+	serviceTable := configData["service_table"].([]interface{})
+	serviceTable = append(serviceTable, map[string]interface{}{
+		"service":         serviceName,
+		"integration_key": integrationKey,
+		"id":              "",
+	})
+	updatedConfigData := sentry.UpdateConfigOrganizationIntegrationsParams{
+		"service_table": serviceTable,
+	}
+	tflog.Debug(ctx, "Updating Sentry PagerDuty Organization Integration with configData", map[string]interface{}{
+		"configData":        updatedConfigData,
+		"new service_table": serviceTable,
+	})
+	_, err = client.OrganizationIntegrations.UpdateConfig(ctx, org, integrationId, &updatedConfigData)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	orgIntegration, _, err = client.OrganizationIntegrations.Get(ctx, org, integrationId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	configData = *orgIntegration.ConfigData
+	var foundServiceRow map[string]interface{}
+	if serviceTable, found := configData["service_table"]; found {
+		for _, row := range serviceTable.([]interface{}) {
+			serviceRow := row.(map[string]interface{})
+			if serviceRow["service"] == serviceName && serviceRow["integration_key"] == integrationKey {
+				foundServiceRow = serviceRow
+				break
+			}
+		}
+	} else {
+		return diag.Errorf("Unable to find PagerDuty service_table information. Is %s a PagerDuty integration?", integrationId)
+	}
+	if foundServiceRow == nil {
+		return diag.Errorf("Unable to find PagerDuty service %s", serviceName)
+	}
+
+	d.SetId(buildThreePartID(org, integrationId, string(foundServiceRow["id"].(json.Number))))
+
+	return resourceSentryOrganizationIntegrationPagerdutyRead(ctx, d, meta)
+}
+
+func resourceSentryOrganizationIntegrationPagerdutyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*sentry.Client)
+	org, integrationId, internalId, err := splitThreePartID(d.Id(), "organization-slug", "integration-id", "service-id")
+	if err != nil {
+		diag.FromErr(err)
+	}
+
+	tflog.Debug(ctx, "Reading Sentry PagerDuty Organization Integration", map[string]interface{}{
+		"org":            org,
+		"integration_id": integrationId,
+		"internal_id":    internalId,
+	})
+	orgIntegration, _, err := client.OrganizationIntegrations.Get(ctx, org, integrationId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	configData := *orgIntegration.ConfigData
+	serviceTable := configData["service_table"].([]interface{})
+	tflog.Debug(ctx, "Found serviceTable", map[string]interface{}{
+		"serviceTable": serviceTable,
+	})
+
+	var foundServiceRow map[string]interface{}
+	for _, row := range serviceTable {
+		serviceRow := row.(map[string]interface{})
+		if string(serviceRow["id"].(json.Number)) == internalId {
+			foundServiceRow = serviceRow
+			break
+		}
+	}
+
+	if foundServiceRow != nil {
+		internalId := string(foundServiceRow["id"].(json.Number))
+		d.SetId(buildThreePartID(org, integrationId, internalId))
+		retErr := multierror.Append(
+			d.Set("organization", org),
+			d.Set("integration_id", integrationId),
+			d.Set("service_name", foundServiceRow["service"]),
+			d.Set("integration_key", foundServiceRow["integration_key"]),
+			d.Set("internal_id", internalId),
+		)
+		return diag.FromErr(retErr.ErrorOrNil())
+	}
+
+	tflog.Info(ctx, "Removing PagerDuty service from state because it no longer exists in Sentry", map[string]interface{}{"id": d.Id()})
+	d.SetId("")
+	return nil
+}
+
+func resourceSentryOrganizationIntegrationPagerdutyUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*sentry.Client)
+	serviceName := d.Get("service_name")
+	integrationKey := d.Get("integration_key")
+	org, integrationId, internalId, err := splitThreePartID(d.Id(), "organization-slug", "integration-id", "service-id")
+	if err != nil {
+		diag.FromErr(err)
+	}
+
+	tflog.Debug(ctx, "Updating Sentry PagerDuty Organization Integration", map[string]interface{}{
+		"org":            org,
+		"integration_id": integrationId,
+		"internal_id":    internalId,
+	})
+	orgIntegration, _, err := client.OrganizationIntegrations.Get(ctx, org, integrationId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	configData := *orgIntegration.ConfigData
+	serviceTable := configData["service_table"].([]interface{})
+	tflog.Debug(ctx, "Found serviceTable", map[string]interface{}{
+		"serviceTable": serviceTable,
+	})
+
+	foundIndex := -1
+	var serviceRow map[string]interface{}
+	for index, row := range serviceTable {
+		serviceRow = row.(map[string]interface{})
+		if string(serviceRow["id"].(json.Number)) == internalId {
+			foundIndex = index
+			break
+		}
+	}
+	if foundIndex >= 0 {
+		serviceTable[foundIndex] = map[string]interface{}{
+			"service":         serviceName,
+			"integration_key": integrationKey,
+			"id":              json.Number(internalId),
+		}
+	} else {
+		return diag.Errorf("Unable to find PagerDuty service with id %s.", internalId)
+	}
+
+	updatedConfigData := sentry.UpdateConfigOrganizationIntegrationsParams{
+		"service_table": serviceTable,
+	}
+	tflog.Debug(ctx, "Updating Sentry PagerDuty Organization Integration with configData", map[string]interface{}{
+		"configData":        updatedConfigData,
+		"new service_table": serviceTable,
+	})
+	_, err = client.OrganizationIntegrations.UpdateConfig(ctx, org, integrationId, &updatedConfigData)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(buildThreePartID(org, integrationId, internalId))
+
+	return resourceSentryOrganizationIntegrationPagerdutyRead(ctx, d, meta)
+}
+
+func resourceSentryOrganizationIntegrationPagerdutyDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*sentry.Client)
+	org, integrationId, internalId, err := splitThreePartID(d.Id(), "organization-slug", "integration-id", "service-id")
+	if err != nil {
+		diag.FromErr(err)
+	}
+
+	tflog.Debug(ctx, "Deleting Sentry PagerDuty Organization Integration", map[string]interface{}{
+		"org":            org,
+		"integration_id": integrationId,
+		"internal_id":    internalId,
+	})
+	orgIntegration, _, err := client.OrganizationIntegrations.Get(ctx, org, integrationId)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	configData := *orgIntegration.ConfigData
+	serviceTable := configData["service_table"].([]interface{})
+	foundIndex := -1
+	var serviceRow map[string]interface{}
+	for index, row := range serviceTable {
+		serviceRow = row.(map[string]interface{})
+		if string(serviceRow["id"].(json.Number)) == internalId {
+			foundIndex = index
+			break
+		}
+	}
+	if foundIndex < 0 {
+		return diag.Errorf("Unable to find PagerDuty service with id %s.", internalId)
+	}
+
+	updatedServiceTable := append(serviceTable[:foundIndex], serviceTable[foundIndex+1:]...)
+	updatedConfigData := sentry.UpdateConfigOrganizationIntegrationsParams{
+		"service_table": updatedServiceTable,
+	}
+	tflog.Debug(ctx, "For Delete: Updating Sentry PagerDuty Organization Integration with configData", map[string]interface{}{
+		"configData": updatedConfigData,
+	})
+	_, err = client.OrganizationIntegrations.UpdateConfig(ctx, org, integrationId, &updatedConfigData)
+	return diag.FromErr(err)
+}
+
+func importSentryOrganizationIntegrationPagerduty(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+
+	org, integrationId, internalId, err := splitThreePartID(d.Id(), "organization-slug", "integration-id", "service-id")
+	if err != nil {
+		return nil, err
+	}
+
+	d.Set("internal_id", internalId)
+	d.Set("integration_id", integrationId)
+	d.Set("organization", org)
+
+	resourceSentryOrganizationIntegrationPagerdutyRead(ctx, d, meta)
+
+	return []*schema.ResourceData{d}, nil
+}
---
+++ sentry/resource_sentry_organization_integration_pagerduty_test.go
@@ -0,0 +1,153 @@
+package sentry
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/jianyuan/go-sentry/v2/sentry"
+)
+
+func TestAccSentryOrganizationIntegrationPagerduty_basic(t *testing.T) {
+	integrationId := "177025" // TODO: use sentry_organization_integration and an env variable to gate running these tests
+	serviceName := acctest.RandomWithPrefix("tf-pagerduty-service")
+	integrationKey := acctest.RandomWithPrefix("tf-pagerduty-integration-key")
+	rn := "sentry_organization_integration_pagerduty.test"
+
+	var serviceID string
+
+	check := func(serviceName, integrationKey string) resource.TestCheckFunc {
+		fs := resource.ComposeTestCheckFunc(
+			testAccCheckSentryOrganizationIntegrationPagerdutyExists(rn, &serviceID),
+			resource.TestCheckResourceAttr(rn, "organization", testOrganization),
+			resource.TestCheckResourceAttr(rn, "integration_id", integrationId),
+			resource.TestCheckResourceAttr(rn, "service_name", serviceName),
+			resource.TestCheckResourceAttr(rn, "integration_key", integrationKey),
+			resource.TestCheckResourceAttrSet(rn, "internal_id"),
+			resource.TestCheckResourceAttrPtr(rn, "internal_id", &serviceID),
+		)
+		return fs
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccCheckSentryOrganizationIntegrationPagerdutyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSentryOrganizationIntegrationPagerdutyConfig(integrationId, serviceName, integrationKey),
+				Check:  check(serviceName, integrationKey),
+			},
+			{
+				Config: testAccSentryOrganizationIntegrationPagerdutyConfig(integrationId, serviceName+"-renamed", integrationKey),
+				Check:  check(serviceName+"-renamed", integrationKey),
+			},
+			{
+				Config: testAccSentryOrganizationIntegrationPagerdutyConfig(integrationId, serviceName+"-renamed", integrationKey+"-renamed"),
+				Check:  check(serviceName+"-renamed", integrationKey+"-renamed"),
+			},
+			{
+				ResourceName:      rn,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckSentryOrganizationIntegrationPagerdutyDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*sentry.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "sentry_organization_integration_pagerduty" {
+			continue
+		}
+
+		ctx := context.Background()
+		//proj, resp, err := client.Projects.Get(ctx, testOrganization, rs.Primary.ID)
+		org, integrationId, id, err := splitThreePartID(rs.Primary.ID, "organization-slug", "integration-id", "service-id")
+
+		orgIntegration, resp, err := client.OrganizationIntegrations.Get(ctx, org, integrationId)
+		configData := *orgIntegration.ConfigData
+		var foundServiceRow map[string]interface{}
+		if serviceTable, found := configData["service_table"]; found {
+			for _, row := range serviceTable.([]interface{}) {
+				serviceRow := row.(map[string]interface{})
+				if string(serviceRow["id"].(json.Number)) == id {
+					foundServiceRow = serviceRow
+					break
+				}
+			}
+		} else {
+			return errors.New("unable to find PagerDuty service_table information")
+		}
+
+		if err == nil {
+			if foundServiceRow != nil {
+				return errors.New("pagerduty service id still exists")
+			}
+		}
+		if resp.StatusCode != 403 && resp.StatusCode != 404 {
+			return err
+		}
+		return nil
+	}
+	return nil
+}
+
+func testAccCheckSentryOrganizationIntegrationPagerdutyExists(n string, serviceID *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return errors.New("no pagerduty service ID is set")
+		}
+
+		org, integrationId, id, err := splitThreePartID(rs.Primary.ID, "organization-slug", "integration-id", "service-id")
+		if err != nil {
+			return err
+		}
+		client := testAccProvider.Meta().(*sentry.Client)
+		ctx := context.Background()
+		orgIntegration, _, err := client.OrganizationIntegrations.Get(ctx, org, integrationId)
+		configData := *orgIntegration.ConfigData
+		var foundServiceRow map[string]interface{}
+		if serviceTable, found := configData["service_table"]; found {
+			for _, row := range serviceTable.([]interface{}) {
+				serviceRow := row.(map[string]interface{})
+				if string(serviceRow["id"].(json.Number)) == id {
+					foundServiceRow = serviceRow
+					break
+				}
+			}
+		} else {
+			return errors.New("unable to find PagerDuty service_table information")
+		}
+
+		if err != nil {
+			return err
+		}
+		*serviceID = string(foundServiceRow["id"].(json.Number))
+		return nil
+	}
+}
+
+func testAccSentryOrganizationIntegrationPagerdutyConfig(integrationId, serviceName, integrationKey string) string {
+	// TODO: get the integration id from sentry_organization_integration data source
+	return testAccSentryOrganizationDataSourceConfig + fmt.Sprintf(`
+resource "sentry_organization_integration_pagerduty" "test" {
+	organization    = data.sentry_organization.test.id
+	integration_id  = "%s"
+	service_name    = "%s"
+	integration_key = "%s"
+}
+	`, integrationId, serviceName, integrationKey)
+}
