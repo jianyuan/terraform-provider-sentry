bc3143cd08444353209bc9da03693ecca47bdc2c
Jian Yuan Lee <jianyuan@gmail.com>
2026-01-07 11:44:51 +0000

    feat: Add import support for `sentry_project_ownership` and refactor `ImportStateTwoPart` helper.

--- internal/tfutils/import_state.go
+++ internal/tfutils/import_state.go
@@ -8,14 +8,18 @@ import (
 	"github.com/jianyuan/terraform-provider-sentry/internal/diagutils"
 )
 
-func ImportStateTwoPartId(ctx context.Context, part1 string, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	part1Value, id, err := SplitTwoPartId(req.ID, part1, "id")
+func ImportStateTwoPart(ctx context.Context, part1 string, part2 string, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	part1Value, part2Value, err := SplitTwoPartId(req.ID, part1, part2)
 	if err != nil {
 		resp.Diagnostics.Append(diagutils.NewImportError(err))
 		return
 	}
 	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root(part1), part1Value)...)
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), id)...)
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root(part2), part2Value)...)
+}
+
+func ImportStateTwoPartId(ctx context.Context, part1 string, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	ImportStateTwoPart(ctx, part1, "id", req, resp)
 }
 
 func ImportStateThreePartId(ctx context.Context, part1 string, part2 string, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
@@ -39,14 +43,4 @@ func ImportStateFourPartId(ctx context.Context, part1 string, part2 string, part
 	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root(part2), part2Value)...)
 	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root(part3), part3Value)...)
 	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), id)...)
-}
-
-func ImportStateTwoPart(ctx context.Context, part1 string, part2 string, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	part1Value, part2Value, err := SplitTwoPartId(req.ID, part1, part2)
-	if err != nil {
-		resp.Diagnostics.Append(diagutils.NewImportError(err))
-		return
-	}
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root(part1), part1Value)...)
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root(part2), part2Value)...)
 }
